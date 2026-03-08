55d8a47c60c36f00caef337456e0d04508e3800a
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-03-08 09:43:31 -0300

    chore: Move jest config from package.json to jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+
+/** @type {import('jest').Config} */
+export default {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+  ],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+}
