5b226d364680e2baba04626e6462d1ecbcf00d7c
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-03-22 19:36:40 -0300

    fix: Add missing port mapping in run-backend-image.sh

--- scripts/run-backend-image.sh
+++ scripts/run-backend-image.sh
@@ -1,7 +1,7 @@
 IMAGE_NAME=back-end-template
 
 # Run in development
-# podman run -it --rm -v ./.env:/app/.env $IMAGE_NAME
+# podman run -it --rm -p 3000:3000 -v ./.env:/app/.env $IMAGE_NAME
 
 # Run in production
-podman run -d --rm -v ./.env:/app/.env $IMAGE_NAME
+podman run -d --rm -p 3000:3000 -v ./.env:/app/.env $IMAGE_NAME
